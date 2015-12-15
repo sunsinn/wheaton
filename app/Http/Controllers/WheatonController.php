effea1d3886253b4517fc003d2a8cea9a1d6cd6c
Summer Unsinn <sunsinn@radcliffe.harvard.edu>
2015-12-15 12:31:58 -0500

    Last minute tweak

--- app/Http/Controllers/WheatonController.php
+++ app/Http/Controllers/WheatonController.php
@@ -28,187 +28,182 @@ class WheatonController extends Controller {
 
   public function postAdd(Request $request) {
     $this->validate(
+      $request,
+      [
+        'url' => 'required|url',
+        'title' => 'required',
+        'ingredients' => 'required',
+      ]
+    );
+
+    $recipe = new \App\Recipe();
+
+    $recipe->url = $request->url;
+    $recipe->title = $request->title;
+    $recipe->user_id = \Auth::id();
+
+    $recipe->save();
+
+    $ingSave = new \App\Ingredient();
+    $ingSave->ingredientsFromString($request->ingredients, $recipe);
+
+    \Session::flash('flash_message','Recipe added!');
+    return redirect('/edit/'.$recipe->id);
+  }
+
+  public function getEdit($id = null) {
+
+    if(!\Auth::check() ) {
+      \Session::flash('flash_message','You have to be logged in to add or edit recipes');
+      return redirect('/');
+    }
+
+    $recipe = \App\Recipe::find($id);
+    if(is_null($recipe)) {
+      \Session::flash('flash_message','Recipe not found.');
+      return redirect('/search');
+    }
+
+    $ingredients = $recipe->ingredients()->get();
+    $ingString = '';
+    foreach ($ingredients as $ingredient) {
+      $ingString .= $ingredient->name.', ';
+    }
+    $ingString = chop($ingString, ', ');
+    return view('edit')->with(['recipe'=>$recipe, 'ingredients'=>$ingString]);
+  }
+
+  public function postEdit(Request $request) {
+
+    $this->validate(
     $request,
     [
       'url' => 'required|url',
+      'title' => 'required',
       'ingredients' => 'required',
     ]
   );
 
-  $recipe = new \App\Recipe();
+  $recipe = \App\Recipe::find($request->id);
 
   $recipe->url = $request->url;
   $recipe->title = $request->title;
-  $recipe->user_id = \Auth::id();
 
   $recipe->save();
 
+  $ingredients = $request->ingredients;
+  $recipe->ingredients()->detach();
   $ingSave = new \App\Ingredient();
-  $ingSave->ingredientsFromString($request->ingredients, $recipe);
+  $ingSave->ingredientsFromString($ingredients, $recipe);
 
-  \Session::flash('flash_message','Recipe added!');
-  return redirect('/edit/'.$recipe->id);
-}
-
-public function getEdit($id = null) {
-
-  if(!\Auth::check() ) {
-    \Session::flash('flash_message','You have to be logged in to add or edit recipes');
-    return redirect('/');
+  \Session::flash('flash_message','Recipe updated!');
+  return redirect('/edit/'.$request->id);
   }
-  $recipe = \App\Recipe::find($id);
-  if(is_null($recipe)) {
-    \Session::flash('flash_message','Recipe not found.');
+
+  public function delete (Request $request) {
+    $recipe = \App\Recipe::find($request->id);
+    $recipe->ingredients()->detach();
+    $recipe->delete();
+
+    \Session::flash('flash_message','Recipe deleted!');
     return redirect('/search');
   }
 
-  $ingredients = $recipe->ingredients()->get();
-  $ingString = '';
-  foreach ($ingredients as $ingredient) {
-    $ingString .= $ingredient->name.', ';
-  }
-  $ingString = chop($ingString, ', ');
-  return view('edit')->with(['recipe'=>$recipe, 'ingredients'=>$ingString]);
-}
-
-public function postEdit(Request $request) {
-
-  $this->validate(
-  $request,
-  [
-    'url' => 'required|url',
-    'title' => 'required',
-    'ingredients' => 'required',
-  ]
-);
-
-$recipe = \App\Recipe::find($request->id);
-
-$recipe->url = $request->url;
-$recipe->title = $request->title;
-
-$recipe->save();
-
-$ingredients = $request->ingredients;
-$recipe->ingredients()->detach();
-$ingSave = new \App\Ingredient();
-$ingSave->ingredientsFromString($ingredients, $recipe);
-
-\Session::flash('flash_message','Recipe updated!');
-return redirect('/edit/'.$request->id);
-}
-
-public function delete (Request $request) {
-  $recipe = \App\Recipe::find($request->id);
-  $recipe->ingredients()->detach();
-  $recipe->delete();
-
-  \Session::flash('flash_message','Recipe deleted!');
-  return redirect('/search');
-}
-
-public function getSearch () {
-  return view ('search');
-}
-
-public function postSearch(Request $request) {
-
-  $recipes = new \App\Recipe();
-  if (empty($request->title) && empty($request->ingredient)){
-    \Session::flash('flash_message','Please enter a title or ingredient');
+  public function getSearch () {
     return view ('search');
   }
 
-  elseif (!empty($request->title)) {
-    if ($request->mineall ==  'mine') {
-      $recipes = \App\Recipe::where('title','LIKE','%'.$request->title.'%')->where('user_id','=',\Auth::id())->get();
-    }
-    else {
-      $recipes = \App\Recipe::where('title','LIKE','%'.$request->title.'%')->get();
-    }
+  public function postSearch(Request $request) {
 
-    return view ('search')->with('recipes', $recipes);
-  }
-  else  {
-
-    $ingredient = \App\Ingredient::where('name', '=', $request->ingredient)->first();
-
-    ## if ingredient isn't found, search under other names
-    if (empty($ingredient)) {
-      $ingredient = \App\Ingredient::where('parallel_name', '=', $request->ingredient)->first();
-    }
-
-    if (empty($ingredient)) {
-      \Session::flash('flash_message','Ingredient not found');
+    $recipes = new \App\Recipe();
+    if (empty($request->title) && empty($request->ingredient)){
+      \Session::flash('flash_message','Please enter a title or ingredient');
       return view ('search');
     }
 
-    $emptyTrue = $ingredient->recipes()->get();
-    if ($emptyTrue->isEmpty()) {
-      $category = $ingredient->category;
-      $recipes = \App\Recipe::whereHas('ingredients', function ($f) use ($category) {
-        $f->where('category', '=', $category);
-      })->get();
-
-      if ($recipes->isEmpty()) {
-        \Session::flash('flash_message','No recipes found with this ingredient');
-        return view ('search');
+    elseif (!empty($request->title)) {
+      if ($request->mineall ==  'mine') {
+        $recipes = \App\Recipe::where('title','LIKE','%'.$request->title.'%')->where('user_id','=',\Auth::id())->get();
       }
       else {
-        \Session::flash('flash_message','No recipes found with this ingredient - try using it in one of these');
-        return view ('search')->with('recipes', $recipes);
+        $recipes = \App\Recipe::where('title','LIKE','%'.$request->title.'%')->get();
       }
 
+      return view ('search')->with('recipes', $recipes);
+    }
+    else  {
+
+      $ingredient = \App\Ingredient::where('name', '=', $request->ingredient)->first();
+
+      ## if ingredient isn't found, search under other names
+      if (empty($ingredient)) {
+        $ingredient = \App\Ingredient::where('parallel_name', '=', $request->ingredient)->first();
+      }
+
+      if (empty($ingredient)) {
+        \Session::flash('flash_message','Ingredient not found');
+        return view ('search');
+      }
+
+      $emptyTrue = $ingredient->recipes()->get();
+      if ($emptyTrue->isEmpty()) {
+        $category = $ingredient->category;
+        $recipes = \App\Recipe::whereHas('ingredients', function ($f) use ($category) {
+          $f->where('category', '=', $category);
+        })->get();
+
+        if ($recipes->isEmpty()) {
+          \Session::flash('flash_message','No recipes found with this ingredient');
+          return view ('search');
+        }
+        else {
+          \Session::flash('flash_message','No recipes found with this ingredient - try using it in one of these');
+          return view ('search')->with('recipes', $recipes);
+        }
+      }
     }
 
+    if ($request->mineall ==  'mine') {
+      $recipes =  $ingredient->recipes()->where('user_id','=',\Auth::id())->get();
+    }
+    else {
+      $recipes =  $ingredient->recipes()->get();
+    }
 
+    return view ('search')->with('recipes', $recipes);
 
   }
 
-
-  if ($request->mineall ==  'mine') {
-    $recipes =  $ingredient->recipes()->where('user_id','=',\Auth::id())->get();
-  }
-  else {
-    $recipes =  $ingredient->recipes()->get();
+  public function browseRecipes () {
+    $recipes = \App\Recipe::orderBy('title')->get();
+    return view ('show')->with('recipes', $recipes);
   }
 
-  return view ('search')->with('recipes', $recipes);
-
-}
-
-public function browseRecipes () {
-  $recipes = \App\Recipe::orderBy('title')->get();
-  return view ('show')->with('recipes', $recipes);
-}
-
-public function browseMyRecipes () {
-  $recipes = \App\Recipe::where('user_id','=',\Auth::id())->orderBy('title')->get();
-  return view ('show')->with('recipes', $recipes);
-}
-
-public function browseIngredients () {
-  $ingredients = \App\Ingredient::has('recipes')->orderBy('name')->get();
-  return view ('show')->with('ingredients', $ingredients);
-}
-
-public function show ($id = null) {
-  $recipe = \App\Recipe::find($id);
-  $ingredients = $recipe->ingredients()->get();
-  $ingString = '';
-  foreach ($ingredients as $ingredient) {
-    $ingString .= $ingredient->name.', ';
+  public function browseMyRecipes () {
+    $recipes = \App\Recipe::where('user_id','=',\Auth::id())->orderBy('title')->get();
+    return view ('show')->with('recipes', $recipes);
   }
-  $ingString = chop($ingString, ', ');
-  return view ('show')->with(['singleRecipe'=>$recipe, 'ingredientString'=>$ingString]);
 
-}
+  public function browseIngredients () {
+    $ingredients = \App\Ingredient::has('recipes')->orderBy('name')->distinct()->get();
+    return view ('show')->with('ingredients', $ingredients);
+  }
 
-public function showRecipes ($id = null) {
-  $ingredient = \App\Ingredient::find($id);
-  $recipes = $ingredient->recipes()->get();
-  return view ('show')->with('recipes', $recipes);
-}
+  public function show ($id = null) {
+    $recipe = \App\Recipe::find($id);
+    $ingredients = $recipe->ingredients()->get();
+    $ingString = '';
+    foreach ($ingredients as $ingredient) {
+      $ingString .= $ingredient->name.', ';
+    }
+    $ingString = chop($ingString, ', ');
+    return view ('show')->with(['singleRecipe'=>$recipe, 'ingredientString'=>$ingString]);
+  }
 
+  public function showRecipes ($id = null) {
+    $ingredient = \App\Ingredient::find($id);
+    $recipes = $ingredient->recipes()->get();
+    return view ('show')->with('recipes', $recipes);
+  }
 
 }
